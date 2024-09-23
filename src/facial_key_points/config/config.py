836e5bce2cef463576f643667882bdab1db53dd7
RomanK26 <rkasichhwa1@gmail.com>
2024-09-23 23:48:17 +0545

    Update config

--- src/facial_key_points/config/config.py
+++ src/facial_key_points/config/config.py
@@ -1,11 +1,10 @@
-import os
-configuration ={
-    'batch_size' : 16,
-    'model_input_size' : (224, 224),
-    'n_epochs': 1,
-    'learning_rate': 0.001,
-    'saved_path' : 'version1',
-    'train_data_csv_path' : '/Users/romankasichhwa/Desktop/Facial_key_points/data/training_frames_keypoints.csv',
-    'test_data_csv_path': '/Users/romankasichhwa/Desktop/Facial_key_points/data/test_frames_keypoints.csv',
-
+configuration = {
+    "batch_size": 16,
+    "model_input_size": (224, 224),
+    "n_epochs": 10,
+    "learning_rate": 0.001,
+    "saved_path": "version2",
+    "train_data_csv_path": "/home/roman/Facial_key_points/data/training_frames_keypoints.csv",
+    "test_data_csv_path": "/home/roman/Facial_key_points/data/test_frames_keypoints.csv",
+    "description": " Changed loss to nn.SmoothL1Loss",
 }
