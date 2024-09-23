3aeca85486d578dc05e7e894c379f57136ed5508
RomanK26 <rkasichhwa1@gmail.com>
2024-09-23 15:26:25 +0545

    Add config

---
+++ src/facial_key_points/config/config.py
@@ -0,0 +1,10 @@
+configuration ={
+    'batch_size' : 16,
+    'model_input_size' : (224, 224),
+    'n_epochs': 20,
+    'learning_rate': 0.001,
+    'saved_path' : 'Saved/version1',
+    'train_data_csv_path' : 'data/training_frames_keypoints.csv',
+    'test_data_csv_path': 'data/test_frames_keypoints.csv',
+
+}
