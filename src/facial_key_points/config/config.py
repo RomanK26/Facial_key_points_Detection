ff37fd9b1a9a534acd755b6be85721075054a1c3
RomanK26 <rkasichhwa1@gmail.com>
2024-09-23 17:32:18 +0545

    complete

--- src/facial_key_points/config/config.py
+++ src/facial_key_points/config/config.py
@@ -1,10 +1,11 @@
+import os
 configuration ={
     'batch_size' : 16,
     'model_input_size' : (224, 224),
-    'n_epochs': 20,
+    'n_epochs': 1,
     'learning_rate': 0.001,
-    'saved_path' : 'Saved/version1',
-    'train_data_csv_path' : 'data/training_frames_keypoints.csv',
-    'test_data_csv_path': 'data/test_frames_keypoints.csv',
+    'saved_path' : 'version1',
+    'train_data_csv_path' : '/Users/romankasichhwa/Desktop/Facial_key_points/data/training_frames_keypoints.csv',
+    'test_data_csv_path': '/Users/romankasichhwa/Desktop/Facial_key_points/data/test_frames_keypoints.csv',
 
 }
